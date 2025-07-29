4921775de2b3f11500642865b1be99b75f4ad7b7
Jose Lugo <jose.lugo@mesa.com>
2025-07-29 15:50:58 -0600

    Nuevo estatus

--- src/components/IdeaCard/IdeaCard.jsx
+++ src/components/IdeaCard/IdeaCard.jsx
@@ -30,6 +30,7 @@ const IdeaCard = ({ idea }) => {
                     className={`inline-flex items-center px-3 py-1 rounded-full text-sm font-medium 
                     ${status === 'Enviada' ? 'bg-yellow-100 text-yellow-800' :
                         status === 'En proceso' ? 'bg-blue-100 text-blue-800' :
+                        status === 'No aprobada' ? 'bg-red-100 text-red-800':
                         'bg-green-100 text-green-800'}`}
                 >
                     { status || 'Sin estado' }
