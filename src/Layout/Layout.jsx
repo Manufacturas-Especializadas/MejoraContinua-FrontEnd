1b68fa975a6590f79588d2b87b2d5fab0469f374
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-04 14:04:04 -0600

    refactor(component): codigo formateado

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,36 +3,35 @@ import { Navbar } from "../components/Navbar/Navbar";
 import OpcionesIncio from "../components/OpcionesIncio/OpcionesIncio";
 
 const Layout = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    const showHeader = location.pathname === '/';
+  const showHeader = location.pathname === "/";
 
-    return (
-        <>
-            <Navbar />
+  return (
+    <>
+      <Navbar />
 
-            {/* Contenido principal */}
-            <div className="max-w-2xl mx-auto px-4">
-                {showHeader && (
-                    <header className="mb-8 text-center pt-5">
-                        <h1 className="text-3xl font-bold text-gray-800">
-                            Plataforma de Mejora Continua
-                        </h1>
-                        <p className="mt-2 text-sm text-gray-600">
-                            Registra tus ideas o consulta su estado.
-                        </p>
-                    </header>
-                )}
+      {/* Contenido principal */}
+      <div className="max-w-2xl mx-auto px-4">
+        {showHeader && (
+          <header className="mb-8 text-center pt-5">
+            <h1 className="text-3xl font-bold text-gray-800">
+              Plataforma de Mejora Continua
+            </h1>
+            <p className="mt-2 text-sm text-gray-600">
+              Registra tus ideas o consulta su estado.
+            </p>
+          </header>
+        )}
 
-                {location.pathname === '/' && <OpcionesIncio />}
+        {location.pathname === "/" && <OpcionesIncio />}
+      </div>
+      {/* Rutas dinámicas */}
+      <main className="mt-5 mx-auto px-4">
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
-            </div>
-            {/* Rutas dinámicas */}
-            <main className="mt-5 mx-auto px-4">
-                <Outlet />
-            </main>
-        </>
-    )
-}
-
-export default Layout
+export default Layout;
